48b9f7e511ccbafcc617cf0c0da20373ce26fd74
Linus Graybill <graybill@gmail.com>
2011-06-08 21:11:56 -0400

    Some copy updates

--- bot.rb
+++ bot.rb
@@ -24,21 +24,26 @@ Net::TOC.new(AIM_USERNAME, AIM_PASSWORD) do |msg, buddy|
   # Log the buddy name, message, and time
   p "#{buddy}: #{msg} at #{Time.now}"
   
+  if msg.include? 'hey'
+    buddy.send_im 'yo, sup'
+  end
+  
   # Save buddy and last timestamp message
   # Intro message, only show if it's a newish IM  
   unless buddies.has_key?(buddy) #&& buddies[buddy] < Time.now - 60*20 #twenty minues ago
-    buddy.send_im "Hi, I'm the RTP Kegerator, I can tell you about the beers I have on tap."
+    buddy.send_im "Hi, I'm the RedTettemer + Partners Kegerator, I can tell you about the beers I have on tap."
     buddy.send_im "On three taps we've got #{beers.map{|b| b + ", " }}."
     buddy.send_im "Type the <b>name of a beer</b> and I'll tell you more about it."
-    buddy.send_im "Type the <b>help</b> and I'll tell you more about what I can do."
+    buddy.send_im "Type <b>help</b> and I'll tell you more about what I can do."
   end
   
   if msg.include? 'help'
     buddy.send_im "<b>You stupid wanker. I can't do shit yet.</b>"
-    buddy.send_im "Get more beer info: "
+    buddy.send_im "We have: "
     beers.each do |b|
       buddy.send_im b
     end
+    buddy.send_im "But "
   end
 
   # turn the beer name into an array and test the msg for each word
